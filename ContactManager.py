731b5567160a7ef3117e0a2299c262367080f573
samuel <oriaku.samuel@gmail.com>
2017-09-18 20:23:57 +0000

    Update on the contact manager

--- ContactManager.py
+++ ContactManager.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class ContactManager:
     def __init__(self, name="", phone_number="", email="", website="", b_day="", picture=""):
         self.name = name
@@ -7,21 +10,99 @@ class ContactManager:
         self.b_day = b_day
         self.picture = picture
         self.value = {}
+        self.all_contact = []
 
-    def create_contact(self):
+    def add_contact(self, contact={}):
 
+        self.all_contact.append(contact)
+
+        print("\n")
+        print(self.all_contact)
+
+    def collect_contact(self):
+        self.value = {}
+        print("\nPlease enter a new contact")
         requests = ['Name', 'Phone Number', 'Email', 'Website', 'Birthday', 'Picture']
         for request in requests:
             self.value[request] = str(input("Please enter your {}: ".format(request)))
+        self.add_contact(self.value)
 
-        print("""Thank you {Name}
-        Your Phone Number is: {Phone Number}
-        Your Email is: {Email}
-        Your Website is: {Website}
-        Your Birthday is: {Birthday}
-        Your Picture address is: {Picture}
-        """.format(**self.value))
+    def delete_contact(self, contact_name):
+        # Delete a contact by name
+        contacts = self.get_contact()
+        for index, name in enumerate(contacts):
+            if contact_name == name['Name']:
+                contacts.pop(index)
+                print("\n Successful removed contact")
+                break
+        else:
+            print("\nPlease brother what is your problem!")
 
+    def search_contact(self, contact_name):
+        contacts = self.get_contact()
+        for index, name in enumerate(contacts):
+            if contact_name == name['Name']:
+                print("""
+ This is the contact you want:
+ Name: {Name}
+ Phone Number:{Phone Number}
+ Email: {Email}
+ Website: {Website}
+ Birthday:{Birthday}
+ Picture:{Picture}
+ """.format(**name))
+                break
+        else:
+            print("\nPlease brother what are you looking for!")
+
+    def get_contact(self):
+        return self.all_contact
+
+    def display_all_contact(self):
+        every_contact = self.get_contact()
+        for contact in every_contact:
+            print("""
+ Name: {Name}
+ Phone Number: {Phone Number}
+ Email: {Email}
+ Website: {Website}
+ Birthday: {Birthday}
+ picture: {Picture}
+            """.format(**contact))
+            print("=" * 40)
+
+    def gensis(self):
+
+        user_selected = ""
+
+        while user_selected is not 'Q':
+
+            print("""
+ A. Add Contact
+ B. Delete Contact
+ C. Search Contact
+ D. Show All Contact
+ Q. Quit
+ """)
+
+            user_selected = str(input("Please Select the Value you want btw A, B, C, D, Q:")).upper()
+
+            if user_selected == "A":
+                # contact_name = str(input("Please Enter a contact name to search f"))
+                self.collect_contact()
+            elif user_selected == "B":
+                contact_name = str(input("\nPlease enter a contact name to delete: "))
+                self.delete_contact(contact_name)
+            elif user_selected == "C":
+                contact_name = str(input("\nPlease enter a contact name you want: "))
+                self.search_contact(contact_name)
+            elif user_selected == "D":
+                print("All Contacts:")
+                self.display_all_contact()
+            elif user_selected == "Q":
+                break
+            else:
+                print("\nPlease enter a valid option")
 
 contact_manager = ContactManager()
-contact_manager.create_contact()
+contact_manager.gensis()
